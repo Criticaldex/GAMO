b7b998f344c6d8ed654a7240b420d6f0f44c0ac3
criticaldex <criticaldex@gmail.com>
2016-04-27 19:19:54 +0200

    cambios en el codigo

--- app/src/main/java/com/example/dam/gamo/benvingutActivity.java
+++ app/src/main/java/com/example/dam/gamo/benvingutActivity.java
@@ -1,13 +1,24 @@
 package com.example.dam.gamo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class benvingutActivity extends AppCompatActivity {
+public class benvingutActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_benvingut);
+        //Button btn = new Button(this);
+        Button btn = (Button) findViewById(R.id.btEntrar);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(benvingutActivity.this, LoginActivity.class));
     }
 }
