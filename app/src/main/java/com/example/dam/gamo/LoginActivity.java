b666bef42f036221bbd89d3ce35fe8910e0a277c
criticaldex <criticaldex@gmail.com>
2016-05-09 09:25:03 +0200

    Login Funcionant

--- app/src/main/java/com/example/dam/gamo/LoginActivity.java
+++ app/src/main/java/com/example/dam/gamo/LoginActivity.java
@@ -2,12 +2,21 @@ package com.example.dam.gamo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.BoolRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,87 +28,80 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Object Exeption;
 
+    public static final String KEY_USERNAME="username";
+    public static final String KEY_PASSWORD="password";
+
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+
+    private String username;
+    private String password;
+    public String LOGIN_URL = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button btLogin = (Button) findViewById(R.id.btLogin);
-        Button btReg = (Button) findViewById(R.id.btRegistrarse);
-        btReg.setOnClickListener(this);
-        btLogin.setOnClickListener(this);
 
+        editTextUsername = (EditText) findViewById(R.id.etUser);
+        editTextPassword = (EditText) findViewById(R.id.etPassword);
+
+        buttonLogin = (Button) findViewById(R.id.btLogin);
+
+        buttonLogin.setOnClickListener(this);
+    }
+
+
+    private void userLogin() {
+        username = editTextUsername.getText().toString().trim();
+        password = editTextPassword.getText().toString().trim();
+        LOGIN_URL = "http://10.0.2.2/gamo/API/users/valid.php?email=aaa&password=aaa";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.trim().equals("true")){
+                            openProfile();
+                        }else{
+                            Toast.makeText(LoginActivity.this,response,Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this,error.toString(),Toast.LENGTH_LONG ).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map = new HashMap<String,String>();
+                map.put(KEY_USERNAME,username);
+                map.put(KEY_PASSWORD,password);
+                return map;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void openProfile(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(KEY_USERNAME, username);
+        startActivity(intent);
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btLogin) {
-            EditText etMail = (EditText) findViewById(R.id.etUser);
-            EditText etPass = (EditText) findViewById(R.id.etPassword);
-
-            boolean response = verifica(etPass.getText().toString(), etMail.getText().toString());
-            if (response == true) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            } else if (response == false) {
-                Context context = getApplicationContext();
-                CharSequence text = "Email or password doesnt match";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            } else {
-                Context context = getApplicationContext();
-                CharSequence text = "falla tot";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-
-        } else
-            if (v.getId() == R.id.btRegistrarse) {
-                startActivity(new Intent(LoginActivity.this, RegistreActivity.class));
-            }
-        }
-
-
-    protected  boolean  verifica(String password, String mail ) {
-        String str = "localhost/GAMO_WEB-master/API/users/valid.php?email=" + mail + "&password=" + password;
-
-        URL url = null;
-        try {
-            url = new URL(str);
-            URLConnection urlc = url.openConnection();
-            BufferedReader bfr = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
-            String line;
-            String response = "";
-            JSONObject jo = null;
-            while ((line = bfr.readLine()) != null) {
-                JSONArray jsa = new JSONArray(line);
-                for (int x = 0; x < jsa.length(); x++) {
-                    jo = (JSONObject) jsa.get(x);
-                }
-                response = jo.toString();
-
-            }
-
-            if (response.trim().equals("true")) {
-                return true;
-            } else {
-                return false;
-            }
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
+        userLogin();
     }
 }
--- app/src/main/java/com/example/dam/gamo/MainActivity.java
+++ app/src/main/java/com/example/dam/gamo/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.dam.gamo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tvNom);
+        Intent intent = getIntent();
+
+        textView.setText(intent.getStringExtra(LoginActivity.KEY_USERNAME));
     }
 }
